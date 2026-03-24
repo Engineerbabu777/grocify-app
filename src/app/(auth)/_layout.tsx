ceec3f6b342c53d8df804d99fa409bb1e761c16f
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-03-24 13:51:36 +0500

    add: signin screen has been added and designed and wokring fine

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -3,8 +3,7 @@ import { Stack } from "expo-router";
 export default function AuthLayout() {
   return (
     <Stack>
-      <Stack.Screen name="sign-in" options={{ headerShown: true }} />
-      <Stack.Screen name="sign-up" options={{ headerShown: true }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
     </Stack>
   );
 }
